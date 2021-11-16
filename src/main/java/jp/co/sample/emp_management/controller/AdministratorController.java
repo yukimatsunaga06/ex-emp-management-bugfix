68e165275ace7a491e8c89a9ad5c0bdb770941c6
yukimatsunaga <yuki.matsunaga@rakus-partners.co.jp>
2021-11-16 11:40:14 +0900

    1-2firstcommit

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jp.co.sample.emp_management.domain.Administrator;
 import jp.co.sample.emp_management.form.InsertAdministratorForm;
@@ -18,7 +20,7 @@ import jp.co.sample.emp_management.service.AdministratorService;
 /**
  * 管理者情報を操作するコントローラー.
  * 
- * @author igamasayuki
+ * @author matsunagayuki
  *
  */
 @Controller
@@ -72,7 +74,10 @@ public class AdministratorController {
 	 * @return ログイン画面へリダイレクト
 	 */
 	@RequestMapping("/insert")
-	public String insert(InsertAdministratorForm form) {
+	public String insert(@Validated InsertAdministratorForm form,BindingResult result,RedirectAttributes redirectAttributes,Model model) { //insertが教科書で言うcreateと同じ
+		if(result.hasErrors()) {
+			return toInsert();
+		}
 		Administrator administrator = new Administrator();
 		// フォームからドメインにプロパティ値をコピー
 		BeanUtils.copyProperties(form, administrator);
--- src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
+++ src/main/java/jp/co/sample/emp_management/form/InsertAdministratorForm.java
@@ -1,17 +1,29 @@
 package jp.co.sample.emp_management.form;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 /**
  * 管理者情報登録時に使用するフォーム.
  * 
- * @author igamasayuki
+ * @author matsunagayuki
  * 
  */
 public class InsertAdministratorForm {
 	/** 名前 */
+	@NotBlank(message="名前は必須です")
 	private String name;
 	/** メールアドレス */
+	@NotBlank(message="Eメールは必須です")
+	@Email(message="メール形式にしてください")
+	@Size(min=8,max=127,message="Eメールは8文字以上127文字以内で記載してください")
 	private String mailAddress;
 	/** パスワード */
+	@NotBlank(message="パスワードは必須です")
+	@Pattern(regexp="^[A-Za-z0-9]+$",message="英数字で入力してください")
+	@Size(min=8,max=127,message="パスワードは8文字以上で記載してください")
 	private String password;
 
 	/**
