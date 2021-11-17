a784ac06777908c79dc3bc2efcbd970d0c69763d
yukimatsunaga <yuki.matsunaga@rakus-partners.co.jp>
2021-11-17 09:50:34 +0900

    (6-1)中級 実務的エラー処理

--- src/main/java/jp/co/sample/emp_management/repository/AdministratorRepository.java
+++ src/main/java/jp/co/sample/emp_management/repository/AdministratorRepository.java
@@ -58,7 +58,7 @@ public class AdministratorRepository {
 	 * @return 管理者情報 存在しない場合はnullを返します
 	 */
 	public Administrator findByMailAddressAndPassward(String mailAddress, String password) {
-		String sql = "select id,name,mail_address,password from administrators where mail_address=:mailAddress and password=:password";  //文字列を''で囲むとSQLが文字列だと判断する
+		String sql = "select id,name,mail_address,password from administrators where mail_address=:mailAddress and password=:password";  //文字列を''で囲むとSQLが文字列だと判断する 
 		SqlParameterSource param = new MapSqlParameterSource().addValue("mailAddress", mailAddress).addValue("password", password);
 		List<Administrator> administratorList = template.query(sql, param, ADMINISTRATOR_ROW_MAPPER);
 		if (administratorList.size() == 0) {
@@ -85,7 +85,7 @@ public class AdministratorRepository {
 	 * @return 管理者情報 存在しない場合はnullを返します
 	 */
 	public Administrator findByMailAddress(String mailAddress) {
-		String sql = "select id,name,mail_address,password from administrators where mail_address=:mailAddress";
+		String sql = "select id,name,mail_address,password from administrators where mail_address=:mailAddress"; //
 		SqlParameterSource param = new MapSqlParameterSource().addValue("mailAddress", mailAddress);
 		List<Administrator> administratorList = template.query(sql, param, ADMINISTRATOR_ROW_MAPPER);
 		if (administratorList.size() == 0) {
